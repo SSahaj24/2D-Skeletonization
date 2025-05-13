d4731564070a05de6540c58481e43159897c2127
Samik Banerjee <samik.banerjee.howrah@gmail.com>
2025-05-13 23:57:54 +0530

    Add files via upload

---
+++ Skektonization_Suite/DM_2D_code/DiMo2d/code/code/dipha-2d-thresh/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/pratik/NEW-CSHL-Suite/DM_2D_code/DiMo2d/code/dipha-2d-thresh
+# Build directory: /home/pratik/NEW-CSHL-Suite/DM_2D_code/DiMo2d/code/dipha-2d-thresh/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
